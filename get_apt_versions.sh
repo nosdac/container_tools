86a93d8f9f9d9dad2dac180f700157457e295b4a
dahlo <m.dahlo@gmail.com>
2019-09-26 11:30:40 +0200

    added comments

--- get_apt_versions.sh
+++ get_apt_versions.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+# get installed apt version of each specified package
+# ex.
+# bash get_apt_versions.sh  wget bedtools bamtools samtools
+
+# do it
 for package in $@ ; do
-
-
     dpkg -s $package | grep Version | echo "$package=$(awk -F ' ' '{print $NF}') \\"
- 
-
 done
 
