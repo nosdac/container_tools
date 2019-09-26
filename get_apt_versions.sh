5e6a336cf337f16cc8cfb8db37413ae2c1ce113e
dahlo <m.dahlo@gmail.com>
2019-09-26 11:41:16 +0200

    added comments

--- get_apt_versions.sh
+++ get_apt_versions.sh
@@ -4,7 +4,9 @@
 # bash get_apt_versions.sh  wget bedtools bamtools samtools
 
 # do it
+printf "RUN apt-get install -y\t\ "
 for package in $@ ; do
-    dpkg -s $package | grep Version | echo "$package=$(awk -F ' ' '{print $NF}') \\"
+    dpkg -s $package | grep Version | printf "\n\t\t\t\t\t\t$package=$(awk -F ' ' '{print $NF}') \\"
 done
+printf "\n"
 
